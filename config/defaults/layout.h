16a93ec96ddacedd2982cb23d43e92154f4ffac4
Hannes Hasselbring <49184341+HannesHasselbring@users.noreply.github.com>
2023-08-16 01:01:38 +0200

    layout like my qmk layout

--- config/defaults/layout.h
+++ config/defaults/layout.h
@@ -13,12 +13,12 @@
 #define DEFAULT_LEFT_THUMB  &mt RGUI SQT    &lt 1 RET
 #define DEFAULT_RIGHT_THUMB &lt 2 SPACE  &mt RALT TAB
 
-#define LOWER_LEFT_R1       &kp EXCL   &kp AT      &kp HASH    &kp DLLR        &kp PRCNT
+#define LOWER_LEFT_R1       &kp EXCL    &kp AT      &kp HASH    &kp DLLR        &kp PRCNT
 #define LOWER_RIGHT_R1      &kp CARET   &kp AMPS    &kp ASTRK   &kp LPAR        &kp RPAR 
 #define LOWER_LEFT_R2       &kp UNDER   &kp PLUS    &kp LBRC    &kp RBRC        &kp PIPE
 #define LOWER_RIGHT_R2      &kp LEFT    &kp DOWN    &kp UP      &kp RIGHT       &kp SEMI 
-#define LOWER_LEFT_R3       &kp ESC     &kp LGUI    &kp LALT    &caps_word      &kp DQT 
-#define LOWER_RIGHT_R3      &kp HOME    &kp END     &kp PAGE_UP &kp PAGE_DOWN   &kp PSCRN
+#define LOWER_LEFT_R3       &kp ESC     &kp LCTRL   &kp LALT    &kp LGUI        &kp DQT 
+#define LOWER_RIGHT_R3      &kp HOME    &kp END     &kp PAGE_UP &kp PAGE_DOWN   &caps_word
 #define LOWER_LEFT_THUMB    &trans      &trans 
 #define LOWER_RIGHT_THUMB   &mo 3       &trans
 
@@ -26,8 +26,8 @@
 #define RAISE_RIGHT_R1      &kp N6          &kp N7      &kp N8      &kp N9      &kp N0 
 #define RAISE_LEFT_R2       &kp MINUS       &kp EQUAL   &kp LBKT    &kp RBKT    &kp BSLH 
 #define RAISE_RIGHT_R2      &kp LG(GRAVE)   &trans      &kp GRAVE   &kp TILDE   &kp COLON 
-#define RAISE_LEFT_R3       &kp ESC         &kp LGUI    &kp RALT    &kp CLCK    &kp DQ
-#define RAISE_RIGHT_R3      &kp TAB         &trans      &trans      &trans      &trans
+#define RAISE_LEFT_R3       &kp ESC         &kp LCTRL   &kp RALT    &kp LGUI    &kp DQ
+#define RAISE_RIGHT_R3      &kp TAB         &trans      &kp RCTRL   &kp RALT    &kp ENTER
 #define RAISE_LEFT_THUMB    &trans  &mo 3 
 #define RAISE_RIGHT_THUMB   &trans  &trans
 
@@ -35,7 +35,7 @@
 #define ADJUST_RIGHT_R1     &bt BT_SEL 0    &bt BT_SEL 1 &bt BT_SEL 2   &bt BT_SEL 3    &bt BT_SEL 4 
 #define ADJUST_LEFT_R2      &kp F6          &kp F7      &kp F8          &kp F9          &kp F10
 #define ADJUST_RIGHT_R2     &none           &none       &none           &none           &none
-#define ADJUST_LEFT_R3      &kp F12         &kp F11     &none           &none           &macro_screenshot 
-#define ADJUST_RIGHT_R3     &kp LG(TAB)     &kp LG(GRAVE) &none         &none           &sys_reset
+#define ADJUST_LEFT_R3      &kp F11         &kp F12     &none           &none           &macro_screenshot 
+#define ADJUST_RIGHT_R3     &kp LG(TAB)     &kp LG(GRAVE) &none         &sys_reset      &bootloader
 #define ADJUST_LEFT_THUMB   &trans  &trans
 #define ADJUST_RIGHT_THUMB  &trans  &trans
